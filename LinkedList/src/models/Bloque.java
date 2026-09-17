10ac0814fe391228cd6c068a69e0b41ca6eb3eef
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-09-17 18:14:39 -0500

    update Bloque.java

--- LinkedList/src/models/Bloque.java
+++ LinkedList/src/models/Bloque.java
@@ -1,6 +1,6 @@
 
 package models;
-
+import java.util.Objects;
 /**
  *
  * @author alejandro
@@ -9,13 +9,13 @@ public class Bloque {
     int id;
     String datos;
     String hashAnterior;
-    //String hashActual;
+    String hashActual;
 
     public Bloque(int id, String datos, String hashAnterior) {
         this.id = id;
         this.datos = datos;
         this.hashAnterior = hashAnterior;
-        //this.hashActual = hashActual;
+        this.hashActual = Integer.toHexString(Objects.hash(id, datos, hashAnterior));
     }
     
     
