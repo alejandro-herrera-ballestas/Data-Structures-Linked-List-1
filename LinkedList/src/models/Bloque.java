253067d9d3dc558292b1902b28e4a1321293014c
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-09-17 17:17:18 -0500

    update Bloque.java

--- LinkedList/src/models/Bloque.java
+++ LinkedList/src/models/Bloque.java
@@ -9,5 +9,18 @@ package models;
  * @author alejandro
  */
 public class Bloque {
+    int id;
+    String datos;
+    String hashAnterior;
+    String hashActual;
+
+    public Bloque(int id, String datos, String hashAnterior, String hashActual) {
+        this.id = id;
+        this.datos = datos;
+        this.hashAnterior = hashAnterior;
+        this.hashActual = hashActual;
+    }
+    
+    
     
 }
