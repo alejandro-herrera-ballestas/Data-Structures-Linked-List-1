84fe28df0a673d671dd623a6949c7497b8eeca4d
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-09-17 18:38:52 -0500

    update App.java

--- LinkedList/src/app/App.java
+++ LinkedList/src/app/App.java
@@ -10,17 +10,45 @@ public class App {
     public static void main(String args[]){
 	
         Lista<Bloque> l = new Lista<>();
+        
+        //              MENU
         System.out.println("            Menu                ");
         System.out.println("1. Nueva Transaccion");     // create
         System.out.println("2. Ver transacciones");     // read
         System.out.println("3. Actualizar (Limitado)");     // update
         System.out.println("4. Eliminar (limitado)");       // delete
         System.out.println("5. Buscar");        // search
-        System.out.println("6. Guardar");      // save, guarda en un archivo
-        System.out.println("7. Abrir");     // open, abre el archivo que decribe los bloques blockchain
+        System.out.println("6. Guardar archivo");      // save, guarda en un archivo
+        System.out.println("7. Abrir archivo");     // open, abre el archivo que decribe los bloques blockchain
+        System.out.println("0. Salir");
         System.out.println("Seleccione una opcion: ");
-        Scanner op = new Scanner(System.in);
         
+        Scanner sc = new Scanner(System.in);
+        int op = sc.nextInt();
+        while (op != 0) {
+            switch (op) {
+                case 1: 
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 0:
+                    System.out.println("Saliendo exitosamente del sistema...");
+                        break;
+                default:
+                    System.out.println("Ingrese una opcion valida!");
+                    break;
+            }
+        }
        
     }
 }
