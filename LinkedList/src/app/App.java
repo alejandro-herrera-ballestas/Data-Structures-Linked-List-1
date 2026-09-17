993a89ef6b3f2d0f9778081ef06841d37d65a5d3
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-09-17 17:11:08 -0500

    update App.java

--- LinkedList/src/app/App.java
+++ LinkedList/src/app/App.java
@@ -2,13 +2,20 @@ package app;
 
 import co.edu.unicartagena.list.Lista;
 import co.edu.unicartagena.list.Nodo;
+import models.Bloque;
 
 
 public class App {
     public static void main(String args[]){
 	
-        Lista<Integer> l = new Lista<>();
-	
+        Lista<Bloque> l = new Lista<>();
+        System.out.println("            Menu                ");
+        System.out.println("1. Nueva Transaccion");     // create
+        System.out.println("2. Ver transacciones");     // read
+        System.out.println("3. Actualizar (Limitado)");     // update
+        System.out.println("4. Eliminar (limitado)");       // delete
+        System.out.println("5. Buscar");        // search
+        System.out.println("6. Guardar");      // save, guarda en un archivo
         
     }
 }
