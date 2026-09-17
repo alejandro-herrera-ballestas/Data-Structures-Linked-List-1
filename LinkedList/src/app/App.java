a78e8d9fbcda1cbeb1ceed5baac47edad9c2d6de
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-09-17 17:03:06 -0500

    commit inicial

--- LinkedList/src/app/App.java
+++ LinkedList/src/app/App.java
@@ -7,8 +7,6 @@ import co.edu.unicartagena.list.Nodo;
 public class App {
     public static void main(String args[]){
 	
-        //System.out.println("*Adicionar al Comienzo de la Lista*");
-	
         Lista<Integer> l = new Lista<>();
 	
         
