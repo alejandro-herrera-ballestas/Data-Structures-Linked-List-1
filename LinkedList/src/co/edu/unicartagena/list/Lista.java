fcad2da6fccfb94abcf185c754c0d76512996f0b
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-09-18 16:16:13 +0000

    update the commit message to reflect the changes made in the code. The changes include adding comments to the Blockchain.java, App.java, and Bloque.java files, as well as removing some unnecessary lines of code in the App.java file. The commit message should be clear and concise, summarizing the modifications made to the codebase.

--- LinkedList/src/co/edu/unicartagena/list/Lista.java
+++ LinkedList/src/co/edu/unicartagena/list/Lista.java
@@ -105,7 +105,7 @@ public class Lista <T> implements Iterable<T>{
 						  temp = temp.sig;
 						  band=true;
 						}
-				    if(band){//
+				    if(band){// el valor del nuevo nodo es mayor que el primer nodo
 				    	nuevoNodo.sig =anterior.sig;
 				    	anterior.sig = nuevoNodo;
 				    }
